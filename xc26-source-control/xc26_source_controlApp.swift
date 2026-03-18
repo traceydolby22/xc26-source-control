08a7dca8c055ea405854e0e38d58688ea1c43ac0
Tracey Bernstein <dolbypersonaltrainingmail.com>
2026-03-18 08:22:59 -0700

    Initial Commit

---
+++ xc26-source-control/xc26_source_controlApp.swift
@@ -0,0 +1,17 @@
+//
+//  xc26_source_controlApp.swift
+//  xc26-source-control
+//
+//  Created by Tracey Bernstein on 3/18/26.
+//
+
+import SwiftUI
+
+@main
+struct xc26_source_controlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
